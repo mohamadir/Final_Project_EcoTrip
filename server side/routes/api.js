891a32996a4694db5408d11a574d378615c243ef
hosen <hosenja>
2017-03-22 13:27:43 +0200

    search api

--- server side/routes/api.js
+++ server side/routes/api.js
@@ -4,16 +4,18 @@ var router = express.Router();
 var Attraction = require('../models/attraction');
 
 /* GET users listing. */
-router.get('/attration/getall', function(req, res, next) {
+router.get('/attraction/getall', function(req, res, next) {
   Attraction.find({}, function(err, data){
     if(err) throw err;
     res.json(data);
   })
 });
 
-router.get('/attration/getone', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/attraction/search', function(req, res, next) {
+  console.log(req.body);
 });
+
+
 
 module.exports = router;
 
