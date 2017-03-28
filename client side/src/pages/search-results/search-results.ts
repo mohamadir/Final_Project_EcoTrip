41bd67b3bad39d895fc8d4da0d5494d4d13514e6
mohamed <mohamdib@gmail.com>
2017-03-28 19:52:06 +0200

    update

--- client side/src/pages/search-results/search-results.ts
+++ client side/src/pages/search-results/search-results.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Alert,AlertController,ModalController } from 'ionic-angular';
 import {AttractionSelectPage} from '../attraction-select/attraction-select';
+import localForage from "localforage";
 /*
   Generated class for the SearchResults page.
 
@@ -19,12 +20,19 @@ export class SearchResultsPage {
     res={
         type: ""
     };
+    people;
     public local: any;
   constructor(public navCtrl: NavController, public navParams: NavParams,public alertCtrl: AlertController
     ,public modalCtrl: ModalController) {
    // this.local = new Storage(LocalStorage);
    // this.local.set('didTutorial', 'true');
-
+    localForage.setItem("key", "hi");
+     localForage.getItem("key").then((result) => {
+                this.people = result ? <Array<Object>> result : [];
+                console.log(result);
+            }, (error) => {
+                console.log("ERROR: ", error);
+            });
 
 
   }
